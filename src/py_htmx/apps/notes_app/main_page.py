f498d50892a21faf0f44acfb0d9caef5020fa724
Richard Brearton <richard.brearton@racingbulls.com>
2024-10-26 18:26:32 +0100

    Added left drawer to main page

--- src/py_htmx/apps/notes_app/main_page.py
+++ src/py_htmx/apps/notes_app/main_page.py
@@ -2,11 +2,12 @@
 
 import py_htmx.models as ui
 
-from .common import html_header, nav_bar
+from .common import html_header, left_drawer, nav_bar
 
 body = ui.Body(
     children=[
         nav_bar,
+        left_drawer,
     ]
 )
 main_page_html_document = ui.HtmlDocument(head=html_header, body=body)
