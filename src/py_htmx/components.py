4e04378fa18a962a88968579229764ebd965a752
Richard Brearton <richard.brearton@racingbulls.com>
2024-10-27 21:43:07 +0000

    Added a nav_bar_button component

--- src/py_htmx/components.py
+++ src/py_htmx/components.py
@@ -1,3 +1,15 @@
-"""Define higher level components that can be easily used to build web apps."""
+"""Create functions that higher level batteries-included components for web apps.
+
+While the html models in `py_htmx.models` are low-level and can be used to build
+any html structure, these components are more opinionated and should look good out of
+the box.
+"""
 
 from . import models as ui
+
+
+def nav_bar_button(text: str, href: str) -> ui.HtmlElement:
+    """Create a nav bar button with text and an icon."""
+    header = ui.Heading(level=4, text=text)
+    article = ui.Article(children=[header])
+    return ui.Anchor(href=href, cls="btn btn-ghost", children=[article])
