3a67beaaa42ba32b9e8b641bbe97caed72b5a2f6
Richard Brearton <richard.brearton@racingbulls.com>
2024-10-27 22:42:35 +0000

    Added some element ids

---
+++ src/py_htmx/apps/notes_app/element_ids.py
@@ -0,0 +1,5 @@
+"""Define some important element ids for the notes app."""
+
+right_drawer_content_div = "right-drawer-content"
+left_drawer_content_div = "left-drawer-content"
+main_content_div = "main-content"
