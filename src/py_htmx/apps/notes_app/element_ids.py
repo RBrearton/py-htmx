41e86fb869403128750518a173a847aafe17904f
Richard Brearton <richard.brearton@racingbulls.com>
2024-10-27 23:19:31 +0000

    Added some more ids

--- src/py_htmx/apps/notes_app/element_ids.py
+++ src/py_htmx/apps/notes_app/element_ids.py
@@ -3,3 +3,5 @@
 right_drawer_content_div = "right-drawer-content"
 left_drawer_content_div = "left-drawer-content"
 main_content_div = "main-content"
+right_drawer_toggle_id = "right-drawer-toggle"
+left_drawer_toggle_id = "left-drawer-toggle"
