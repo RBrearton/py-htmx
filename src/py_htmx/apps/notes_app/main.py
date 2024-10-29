5c1960e96376efaff39de848ac0bb9819d912c33
Richard Brearton <richard.brearton@racingbulls.com>
2024-10-29 21:49:55 +0000

    Added first pass at a working B6 PS1 page

--- src/py_htmx/apps/notes_app/main.py
+++ src/py_htmx/apps/notes_app/main.py
@@ -1,28 +1,71 @@
 """Defines the FastAPI routes. Running will run the FastAPI server."""
 
+from functools import lru_cache
+
 import uvicorn
+from bs4 import BeautifulSoup
 from fastapi import FastAPI, HTTPException, Request
 from fastapi.responses import FileResponse, HTMLResponse
 
 from py_htmx import components as c
 from py_htmx import models as ui
-from py_htmx.apps.notes_app.common import make_page
+
+from . import endpoint_names as endpoints
+from .common import make_page, make_physics_left_drawer
+from .config import config
 
 app = FastAPI(
     title="Physics teaching notes",
 )
 
 
-@app.get("/dist.css")
+@lru_cache
+def raw_markdown(file_name: str) -> str:
+    """Read the markdown file at the given path."""
+    return (config.markdown_files_dir / file_name).read_text()
+
+
+@app.get(endpoints.css_file)
 async def get_css() -> FileResponse:
     """Return the CSS file."""
     return FileResponse("dist.css")
 
 
-@app.get("/favicon.ico")
+@app.get(endpoints.favicon)
 async def get_favicon() -> FileResponse:
     """Return the favicon."""
     return FileResponse("note_icon.png")
+
+
+@app.get(endpoints.b6_ps1)
+async def get_b6_ps1() -> HTMLResponse:
+    """Return the page for B6 problem set 1.
+
+    This renders the b6_ps1.md markdown file.
+    """
+    # Get the appropriate markdown file.
+    markdown_txt = raw_markdown("b6_ps1.md")
+
+    # Render the markdown file as an html string.
+    rendered_markdown = ui.render_markdown(markdown_txt)
+
+    # Find all of the headings in the rendered markdown.
+    soup = BeautifulSoup(rendered_markdown, "html.parser")
+    headings = soup.find_all(["h1", "h2", "h3", "h4", "h5", "h6"])
+
+    # Create a list of (heading_txt, heading_id) tuples that we can turn into a menu.
+    links: list[tuple[str, str | c.RecursiveList]] = [
+        (heading.text, heading["id"]) for heading in headings
+    ]
+    right_menu = c.contents_menu(links, "Contents")
+
+    # Create the main page.
+    main_page = make_page(
+        main_content=ui.Article(cls="prose", raw_inner_html=rendered_markdown),
+        left_drawer_content=make_physics_left_drawer(),
+        right_drawer_content=right_menu,
+    )
+    return HTMLResponse(content=main_page.model_dump_html())
 
 
 @app.get("/")
@@ -41,13 +84,7 @@ async def get_index() -> HTMLResponse:
                 ),
             ],
         ),
-        left_drawer_content=c.contents_menu(
-            [
-                ("B2: Symmetry and relativity", [("PS1", "#"), ("PS2", "#")]),
-                ("B6: Condensed matter physics", [("PS1", "#"), ("PS2", "#")]),
-            ],
-            "Left title",
-        ),
+        left_drawer_content=make_physics_left_drawer(),
         right_drawer_content=c.contents_menu(
             [("right_drawer_1", "#"), ("right_drawer_2", "#")], "Right title"
         ),
