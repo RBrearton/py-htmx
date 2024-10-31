b88d37e571c4ed23dd45a1c08beda13a5035c4dd
Richard Brearton <richard.brearton@racingbulls.com>
2024-10-31 21:42:31 +0000

    Removed unused theme: {}

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["**/*.html", "**/*.py"], // Adjust this to match where your HTML files are located
-  theme: {
-    extend: {},
-  },
   plugins: [require("@tailwindcss/typography"), require("daisyui")],
   daisyui: {
     themes: [
