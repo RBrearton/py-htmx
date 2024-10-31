855927bb40e172bc350a87b47187b4afd8ae44a3
Richard Brearton <richard.brearton@racingbulls.com>
2024-10-31 19:52:46 +0000

    Stopped watching markdown files because it doesn't seem to work

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["**/*.html", "**/*.py", "**/*.md"], // Adjust this to match where your HTML files are located
+  content: ["**/*.html", "**/*.py"], // Adjust this to match where your HTML files are located
   theme: {
     extend: {},
   },
