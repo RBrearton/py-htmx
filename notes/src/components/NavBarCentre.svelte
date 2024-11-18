be4094646b03531b4639a9da41bd3bea9516ab3e
Richard Brearton <richardbrearton@gmail.com>
2024-11-18 22:33:40 +0000

    Added simple NavBarCentre

---
+++ notes/src/components/NavBarCentre.svelte
@@ -0,0 +1,9 @@
+<script>
+  import NavBarButton from "./NavBarButton.svelte";
+</script>
+
+<div class="navbar-center">
+  <NavBarButton text="Home" route="/" />
+  <NavBarButton text="Physics" route="/physics" />
+  <NavBarButton text="Research" route="/research" />
+</div>
