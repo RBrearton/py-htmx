85995a42a837656c35a66d2159005f1902d5c98a
Richard Brearton <richardbrearton@gmail.com>
2024-11-18 22:57:30 +0000

    Made a number of svelteward changes

---
+++ notes/src/components/NavBar.svelte
@@ -0,0 +1,11 @@
+<script>
+  import NavBarCentre from "./NavBarCentre.svelte";
+  import NavBarEnd from "./NavBarEnd.svelte";
+  import NavBarStart from "./NavBarStart.svelte";
+</script>
+
+<div class="navbar bg-primary text-primary-content shadow">
+  <NavBarStart title="Notes" />
+  <NavBarCentre />
+  <NavBarEnd />
+</div>
---
+++ notes/src/components/NavBarStart.svelte
@@ -0,0 +1,16 @@
+<script>
+  // Make a prop called 'title' for our site title.
+  export let title;
+</script>
+
+<div class="navbar-start">
+  <div class="flex flex-row">
+    <!-- The main site icon.  We use google material icons for this. -->
+    <i class="material-icons text-primary-content">notes</i>
+
+    <!-- The main site title. -->
+    <h4 class="text-primary-content">
+      {title}
+    </h4>
+  </div>
+</div>
