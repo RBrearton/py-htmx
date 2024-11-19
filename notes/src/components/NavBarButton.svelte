827f1ac538854de730fb6ce8af3a7639a00e1d97
Richard Brearton <richardbrearton@gmail.com>
2024-11-19 09:29:09 +0000

    NavBarButtons now use NavBarHeadings without padding for consistent style

--- notes/src/components/NavBarButton.svelte
+++ notes/src/components/NavBarButton.svelte
@@ -1,9 +1,11 @@
 <script>
+  import NavBarHeading from "./NavBarHeading.svelte";
+
   // Take one prop for the text, another for the route.
   export let text;
   export let route;
 </script>
 
 <a class="btn btn-ghost" href={route}>
-  {text}
+  <NavBarHeading title={text} isPadded={false} />
 </a>
