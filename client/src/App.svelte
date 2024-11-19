34e287ed35ceaeb599d5d1137ae6399bc764e863
Richard Brearton <richardbrearton@gmail.com>
2024-11-19 19:23:42 +0000

    Moved router so that it's the root component of the app

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -1,7 +1,27 @@
 <script lang="ts">
+  import { onMount } from "svelte";
+  import { Router, Route, Link } from "svelte-routing";
   import NotesPage from "./components/NotesPage.svelte";
-  import Router from "./components/Router.svelte";
+
+  interface RouteModel {
+    name: string;
+    path: string;
+  }
+  let routes: RouteModel[] = [];
+
+  onMount(async () => {
+    const response = await fetch("/api/routes");
+    routes = await response.json();
+  });
 </script>
 
-<Router />
-<NotesPage pageTitle="Home" />
+<Router>
+  <nav>
+    {#each routes as route}
+      <Link to={route.path}>{route.name}</Link>
+    {/each}
+  </nav>
+  {#each routes as route}
+    <Route path={route.path} component={NotesPage} props={{ pageTitle: route.name }} />
+  {/each}
+</Router>
