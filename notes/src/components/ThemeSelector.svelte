269f81d5bee2b352d34bbd45249641e09f0540d8
Richard Brearton <richardbrearton@gmail.com>
2024-11-19 08:42:40 +0000

    Added first pass at a theme selector

---
+++ notes/src/components/ThemeSelector.svelte
@@ -0,0 +1,10 @@
+<label class="flex flex-row cursor-pointer gap-2 px-5">
+  <!-- Add a sun icon for the light mode. -->
+  <i class="material-icons text-primary-content">sunny</i>
+
+  <!-- Add a checkbox input to control the theme. -->
+  <input type="checkbox" class="toggle theme-controller" />
+
+  <!-- Add a moon icon for the dark mode. -->
+  <i class="material-icons text-primary-content">dark_mode</i>
+</label>
