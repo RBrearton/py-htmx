68a4fee8ec833ca30132635ed1e4e05d0b47ae7c
Richard Brearton <richardbrearton@gmail.com>
2024-11-19 09:49:25 +0000

    Added NavBarEnd

---
+++ notes/src/components/NavBarEnd.svelte
@@ -0,0 +1,11 @@
+<script>
+  import GithubLink from "./GithubLink.svelte";
+  import SearchBar from "./SearchBar.svelte";
+  import ThemeSelector from "./ThemeSelector.svelte";
+</script>
+
+<div class="navbar-end">
+  <ThemeSelector />
+  <SearchBar placeholder="Search" />
+  <GithubLink link="https://github.com/rbrearton/py-htmx" />
+</div>
