3e0e9b33e8b558f3deb38b4d07890df9defac663
Richard Brearton <richardbrearton@gmail.com>
2024-11-19 09:24:40 +0000

    Added some options to NavBarHeading

--- notes/src/components/NavBarHeading.svelte
+++ notes/src/components/NavBarHeading.svelte
@@ -1,10 +1,20 @@
 <script>
   export let title;
+  export let isPadded = true;
 </script>
 
 <!-- We wrap the title up in a prose block to improve the formatting/style. -->
-<article class="prose px-5">
-  <h4 class="text-primary-content">
-    {title}
-  </h4>
-</article>
+
+{#if isPadded}
+  <article class="prose px-5">
+    <h4 class="text-primary-content">
+      {title}
+    </h4>
+  </article>
+{:else}
+  <article class="prose">
+    <h4 class="text-primary-content">
+      {title}
+    </h4>
+  </article>
+{/if}
