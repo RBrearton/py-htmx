b930cb42d4ab9f5cdaccc1e2548ffcee7e8c21fa
Richard Brearton <richardbrearton@gmail.com>
2024-11-19 09:48:50 +0000

    Added a link to the github repo

---
+++ notes/src/components/GithubLink.svelte
@@ -0,0 +1,10 @@
+<script>
+  // Import the github logo svg.
+  import githubLogo from "../assets/github.svg";
+
+  export let link;
+</script>
+
+<a href={link} class="px-5">
+  <img src={githubLogo} alt="GitHub logo" class="w-8 h-8 text-primary-content" />
+</a>
