ddd37fbb84e0eab6657008e72fa7107cb8b4ffa1
Richard Brearton <richardbrearton@gmail.com>
2024-11-19 18:59:43 +0000

    Made NavBarCentre dynamic

--- client/src/components/NavBarCentre.svelte
+++ client/src/components/NavBarCentre.svelte
@@ -1,9 +1,26 @@
-<script>
+<script lang="ts">
+  import { onMount } from "svelte";
   import NavBarButton from "./NavBarButton.svelte";
+
+  // Make a typescript interface for the topic that we're fetching a list of.
+  interface Topic {
+    name: string;
+  }
+  let topics: Topic[] = [];
+
+  // Fetch the topics from the server.
+  onMount(async () => {
+    const response = await fetch("/api/topics");
+    topics = await response.json();
+
+    // Log the topics to the console.
+    console.log(topics);
+  });
 </script>
 
 <div class="navbar-center">
   <NavBarButton text="Home" route="/" />
-  <NavBarButton text="Physics" route="/physics" />
-  <NavBarButton text="Research" route="/research" />
+  {#each topics as topic}
+    <NavBarButton text={topic.name} route={`/${topic.name.toLowerCase()}`} />
+  {/each}
 </div>
